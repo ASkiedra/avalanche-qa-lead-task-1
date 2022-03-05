26402843b7eeb6f918f605bc99e9f8df045700f0
Augustas Skiedra <sk.augustas@gmail.com>
2022-03-05 15:51:48 +0200

    settings view

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ const App = () => {
   const [addAmount, setAddAmount] = useState(0)
   const [userData, setUserData] = useState({ birthDate: new Date(), weight: 0, phoneNumber: '' })
   const [registered, setRegistered] = useState(false)
+  const [inSettings, setInSettings] = useState(false)
   const shouldFormat = drankAmount >= 1000;
   const maxAmountDrank = 10000;
   const unit = shouldFormat ? 'l' : 'ml'
@@ -16,34 +17,43 @@ const App = () => {
     <div className="App">
       <div style={{ fontSize: 26 }} className="centered">
         <div id='main-view'>
-          <h1 style={{ marginBottom: '10rem' }}>{registered ? 'Hydration App Functional Prototype' : 'Registration'}</h1>
+          <h1 style={{ marginBottom: '10rem' }}>{(registered && !inSettings) ? 'Hydration App Functional Prototype' : inSettings ? 'Settings' : 'Registration'}</h1>
 
-          {registered ?
+          {(registered && !inSettings) ?
             <>
               <input type='number' min="0" style={{ padding: '1rem' }} value={addAmount || ''} onChange={({ target }) => setAddAmount(+target.value)} />
 
-              <button style={{ marginBottom: '4rem', padding: '1rem' }} onClick={() => {
-                if (addAmount <= 0)
-                  alert('Please enter a value that is above 0.')
-                else if (drankAmount + addAmount > maxAmountDrank)
-                  alert("Please don't drink more than " + maxAmountDrank / 1000 + 'l of liquid')
-                else
-                  setDrankAmount(drankAmount + addAmount)
+              <button
+                aria-label='add-btn'
+                style={{ marginBottom: '4rem', padding: '1rem' }} onClick={() => {
+                  if (addAmount <= 0)
+                    alert('Please enter a value that is above 0.')
+                  else if (drankAmount + addAmount > maxAmountDrank)
+                    alert("Please don't drink more than " + maxAmountDrank / 1000 + 'l of liquid')
+                  else
+                    setDrankAmount(drankAmount + addAmount)
 
-                setAddAmount(0)
-              }}>Add</button>
+                  setAddAmount(0)
+                }}>Add</button>
 
               <p style={{ border: '3px solid cyan', padding: '0.5rem 0', width: '25rem', margin: '0 auto' }}>
                 {shouldFormat ? (drankAmount / 1000).toFixed(1) : drankAmount} {unit}
               </p>
+
+              <button aria-label="settings-button" style={{ borderRadius: '10px', marginTop: '2rem', padding: '0.5rem 1.5rem' }} onClick={() => setInSettings(true)}><b>SETTINGS</b></button>
             </>
             :
             <>
-              <p>Phone number</p>
 
-              <input value={userData.phoneNumber || ''} onChange={({ target }) => {
-                setUserData({ ...userData, phoneNumber: target.value })
-              }} />
+              {!inSettings &&
+                <>
+                  <p>Phone number</p>
+
+                  <input value={userData.phoneNumber || ''} onChange={({ target }) => {
+                    setUserData({ ...userData, phoneNumber: target.value })
+                  }} />
+                </>}
+
 
               <p>Birth date</p>
 
@@ -64,18 +74,23 @@ const App = () => {
                 setUserData({ ...userData, weight: +target.value })
               }} />
 
-
               <div>
-                <button onClick={() => {
-                  if (userData.phoneNumber.length < 6) {
-                    return alert('Phone number too short.')
-                  }
+                <button
+                  aria-label="settings-or-register-button"
+                  onClick={() => {
+                    if (!inSettings && userData.phoneNumber.length < 6) {
+                      return alert('Phone number too short.')
+                    }
 
-                  if (!userData.weight || !userData.birthDate) {
-                    return alert('Please enter all fields.')
-                  }
-                  setRegistered(true)
-                }}>Register</button>
+                    if (!userData.weight || !userData.birthDate) {
+                      return alert('Please enter all fields.')
+                    }
+
+                    if (inSettings)
+                      return setInSettings(false)
+
+                    setRegistered(true)
+                  }}>{inSettings ? 'Save' : 'Register'}</button>
               </div>
             </>
 
